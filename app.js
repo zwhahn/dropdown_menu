0a41f421206c89b5a8858eb8b83a959f66983deb
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-06-02 19:56:25 -0700

    Get dropdown working

---
+++ app.js
@@ -0,0 +1,8 @@
+dropdownBtn = document.getElementById('dropdown-btn');
+dropdownBtn.addEventListener('click', showDropdown);
+
+function showDropdown () {
+    console.log("clicked")
+    dropdownContent = document.getElementById('content');
+    dropdownContent.classList.toggle('show');
+}
